7adad084dc6759b6a566178dbd63031afe58a8ab
smileyyesman <113839438+smileyyesman@users.noreply.github.com>
2022-10-15 10:09:15 +0900

    Update Q1_MID.R

--- Q1_MID.R
+++ Q1_MID.R
@@ -1,21 +1,3 @@
-
-usethis::use_git_config(user.name = "Geonho Lee", user.email = "capgrah@naver.com")
-
-## setting: permanent storage
-credentials::credential_helper_set("manager")                 # Windows
-credentials::credential_helper_set("osxkeychain")             # Mac
-credentials::credential_helper_set("store")                   # Linux
-
-## create a personal access token for authentication:
-usethis::create_github_token() 
-## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
-
-## copy and paste your token and press enter
-credentials::set_github_pat()
-
-
-
-
 library(data.table);library(magrittr);library(ggpubr);library(rvg);library(officer) 
 a <- fread("https://raw.githubusercontent.com/jinseob2kim/R-skku-biohrs/main/data/example_g1e.csv")
 a
